b11c82dce9da8c8296e75ccc15600d40a5a0e5d7
Javier Ruiz <jruiz@zertifica.com>
2017-07-17 17:57:24 +0200

    Creating a smart component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+    <div class="app">      
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `
 })
